d67f63d4475ed3e36cfde6fc331012e288c03636
Lukasz Grajczyk <lukaszgrajczyk1988@gmail.com>
2024-02-16 18:53:43 +0100

    Add StartScreen

---
+++ lib/quiz.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuizScreenManage extends StatelessWidget {
+  const QuizScreenManage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -17,11 +17,34 @@ class StartScreen extends StatelessWidget {
           end: Alignment.topCenter,
         ),
       ),
-      child: const Center(
-          child: Text(
-        'Learn Flutter is the best way',
-        style: TextStyle(color: Colors.white, fontSize: 20),
-      )),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              color: const Color.fromARGB(161, 255, 255, 255),
+              width: 300,
+            ),
+            const SizedBox(height: 40.0),
+            const Text(
+              'Learn Flutter is the best way for you',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
+            ),
+            const SizedBox(height: 40.0),
+            OutlinedButton.icon(
+              icon: const Icon(Icons.accessible_forward_sharp),
+              label: const Text(
+                'Start Quiz',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 254, 254), fontSize: 15),
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
